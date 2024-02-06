2faf1a4d4cb9599b27978166d715d29804ee7afa
Chamath <madhusankachamath@gmail.com>
2024-02-05 17:10:36 -0800

    completed the basic sidebar without effects

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import { Sidebar } from './collections/sidebar';
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Sidebar/>
     </div>
   );
 }
---
+++ frontend/src/collections/sidebarData.js
@@ -0,0 +1,45 @@
+import React from "react";
+import HomeIcon from '@mui/icons-material/Home';
+import OnlinePredictionIcon from '@mui/icons-material/OnlinePrediction';
+import RollerSkatingIcon from '@mui/icons-material/RollerSkating';
+import StorageIcon from '@mui/icons-material/Storage';
+import SettingsIcon from '@mui/icons-material/Settings';
+import LogoutIcon from '@mui/icons-material/Logout';
+
+export const sidebarData = [
+    {
+        title: "My Profile",
+        icon: <HomeIcon/>,
+        link: "profile/"
+    },
+
+    {
+        title: "Demand Prediction",
+        icon: <OnlinePredictionIcon/>,
+        link: "prediction/"
+    },
+
+    {
+        title: "New Models",
+        icon: <RollerSkatingIcon/>,
+        link: "models/"
+    },
+
+    {
+        title: "Update Database",
+        icon: <StorageIcon/>,
+        link: "updateDatabase/"
+    },
+
+    {
+        title: "Settings",
+        icon: <SettingsIcon/>,
+        link: "settings/"
+    },
+
+    {
+        title: "Logout",
+        icon: <LogoutIcon/>,
+        link: "logout/"
+    },
+];
