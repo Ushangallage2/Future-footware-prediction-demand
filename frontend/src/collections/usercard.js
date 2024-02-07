1c698a6671f7cfe89cc562839de88fb66e407cee
sandali24 <sandalisiri58@gmail.com>
2024-02-07 12:47:09 +0530

    Update usercard.js

--- frontend/src/collections/usercard.js
+++ frontend/src/collections/usercard.js
@@ -4,13 +4,21 @@ import userimg from './girl.png';
 
 function Card(props) {
     return (
+      <div class="container">
+
         <div className="card">
           <img src={userimg} alt="card image" className="card-image" />
           <div className="card-content">
-            <h3 className="card-title">{props.title}</h3>
-            <h4 className="card-subtitle">{props.subtitle}</h4>
-            <p className="card-body">{props.body}</p>
+            {/* <h3 className="card-title">{props.title}</h3>
+            <h4 className="card-subtitle">{props.subtitle}</h4> */}
+            <p className="card-body">{props.body1}</p>
+            <p className="card-body">{props.body2}</p>
+            <p className="card-body">{props.body3}</p>
+            <p className="card-body">{props.body4}</p>
+            <p className="card-body">{props.body5}</p>
+            <p className="card-body">{props.body6}</p>
           </div>
+        </div>
         </div>
       );
     }
