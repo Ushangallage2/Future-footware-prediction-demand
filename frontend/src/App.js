488f51e8863bc717d69e5f67599eecf73e87e34e
sandali24 <sandalisiri58@gmail.com>
2024-02-07 08:51:52 +0530

    Edit App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,24 @@
 
 import './App.css';
-import { Sidebar } from './collections/sidebar';
+import { Home } from './collections/home';
+// import { Sidebar } from './collections/sidebar';
+import { UserProf } from './collections/userprof';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
 function App() {
   return (
     <div className="App">
-      <Sidebar/>
-    </div>
+
+<BrowserRouter>
+      {/* <Sidebar/> */}
+      {/* <UserProf/> */}
+      <Routes>
+      <Route path='/' element={<Home/>}/> 
+      <Route path='/userprofile' element={<UserProf/>}/> // use element prop instead of children prop
+          {/* You can add more routes for other sidebar items here */}
+      </Routes>
+      </BrowserRouter>
+      </div>
   );
 }
 
