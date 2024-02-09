c30ed120027be329d154f76a437b262d1110b387
sandali24 <sandalisiri58@gmail.com>
2024-02-09 15:33:59 +0530

    Update home.js

--- frontend/src/collections/pages/homePage/home.js
+++ frontend/src/collections/pages/homePage/home.js
@@ -1,12 +1,22 @@
 import React from 'react';
+<<<<<<< HEAD:frontend/src/collections/pages/homePage/home.js
 import { Sidebar } from '../../sidebar/sidebar';
+=======
+// import { Sidebar } from './sidebar';
+import './home.css';
+import "boxicons/css/boxicons.min.css";
+import 'remixicon/fonts/remixicon.css'
+import logo from './fdfs.png';
+
+>>>>>>> dev-sandali:frontend/src/collections/home.js
 
 
 export function Home(props) {
+    
     return (
         <>
-            <Sidebar/>
-            {/* <h1>Home</h1> */}
+        <img className= 'slidebarIcon' src={logo} alt = "logo" />
+        {/* <a class="logo">FDFS</a> */}
         </>
         
     )
