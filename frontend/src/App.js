b959b62e4e9f729570dba77995d7d1177f4b965d
Asal Samarasinghe <104274406+Asal30@users.noreply.github.com>
2024-02-15 05:05:58 +0530

    creating login page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,14 +5,20 @@ import Home from './collections/pages/homePage/home';
 import { UserProf } from './collections/pages/userProfile/userprof';
 import { NewModel } from './collections/pages/newModelPage/newmodel';
 import { Settings } from './collections/pages/settingsPage/settings';
-// import Settings from './settings';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+// import Login from './collections/pages/loginPage/loginPage';
+import React, { useState } from 'react';
 
 function App() {
   return (
+  //   <div>
+  //   <h1>My React Login App</h1>
+  //   <Login />
+  // </div>
+    
     <div className="App">
-
-<BrowserRouter>
+ <BrowserRouter>
       <Routes>
       <Route path='/' element={<Home/>}/> 
       <Route path='/userprofile' element={<UserProf/>}/>
