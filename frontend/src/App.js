90432f3427f1d0c11301ce0be8b7db1c0d38d56e
Chamath <madhusankachamath@gmail.com>
2024-02-14 22:47:48 -0800

    fixed the errors

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -21,11 +21,8 @@ function App() {
       <Route path='/' element={<Home/>}/> 
       <Route path='/userprofile' element={<UserProf/>}/>
       <Route path='/settings' element={<Settings/>}/> 
-<<<<<<< HEAD
       <Route path='/demandPrediction' element={<DemandPrediction />} />
-=======
       <Route path='/newmodel' element={<NewModel/>}/> 
->>>>>>> dev-asal30
       </Routes>
       </BrowserRouter>
       </div>
