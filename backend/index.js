9846de479eee558ac2e02b5316036432a4dc683d
savisaluwadana <82082138+savisaluwadana@users.noreply.github.com>
2024-03-01 10:10:56 +0530

    test commit

---
+++ backend/index.js
@@ -0,0 +1,28 @@
+// Make express server
+const express = require("express");
+const app = express();
+const cors = require('cors')
+//const {admin} = require("./service/initializeUser")
+const port = process.env.PORT || 8080;
+app.use(express.json());
+
+app.use(cors());
+
+const userRoutes = require("./routes/User");
+app.use("/user", userRoutes);
+
+const authRoutes = require("./routes/Auth");
+app.use("/auth", authRoutes);
+ 
+
+const demandPredictRoutes = require("./routes/Demandprediction");
+app.use("/demandpred", demandPredictRoutes);
+
+//admin()
+
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`); 
+  console.log(`http://localhost:${port}`)
+});
+
