e44977b144d324b88b9c39caf64fb0b7200d314b
Asal Samarasinghe <104274406+Asal30@users.noreply.github.com>
2024-03-15 18:42:27 +0530

    minor changes

--- frontend/src/collections/components/UsernameTypewriter.js
+++ frontend/src/collections/components/UsernameTypewriter.js
@@ -1,57 +1,8 @@
-// import React from 'react';
-// import Typewriter from 'react-typewriter-effect';
-// import { useUser } from './UserContext';
-
-// const UsernameTypewriter = () => {
-//   const { currentUser } = useUser();
-//   console.log("its here toooooo!")
-//   console.log(currentUser)
-
-//   return (
-//     <div className="username-typewriter">
-//       <Typewriter
-//         options={{
-//           strings: [currentUser?.username || ''],
-//           autoStart: true,
-//           loop: false,
-//         }}
-//       />
-//     </div>
-//   );
-// };
-
-// export default UsernameTypewriter;
-// import React from 'react';
-// import { Typewriter } from 'react-simple-typewriter';
-// import { useUser } from './UserContext';
-// import './usernameTypewriter.css';
-
-// const UsernameTypewriter = () => {
-//   const { currentUser } = useUser();
-
-//   return (
-//     <div className="username-typewriter" >
-//       <Typewriter
-//         words={[currentUser?.username || '']}
-//         loop={true}
-//         cursor={true}
-//         cursorStyle='_'
-//         typeSpeed={50}
-//         deleteSpeed={50}
-//         delaySpeed={1000}
-        
-//       />
-//     </div>
-//   );
-// };
-
-// export default UsernameTypewriter;
-
 import React from 'react';
+import '../../App.css';
 import { Typewriter } from 'react-simple-typewriter';
 import { useUser } from './UserContext';
 import './usernameTypewriter.css';
-
 
 
 
