0941f133fa2c9ca31d02b32bb2c422296f136c96
savisaluwadana <82082138+savisaluwadana@users.noreply.github.com>
2024-03-23 15:16:28 +0530

    commit

--- backend/index.js
+++ backend/index.js
@@ -80,7 +80,7 @@ io.on("connection", (socket) => {
 // const {admin} = require("./service/initializeUser")
 const port = process.env.PORT || 8080;
 app.use(express.json());
-app.use("/viewReport", viewreport);
+
 app.use(express.urlencoded({ extended: false }));
 
 const userRoutes = require("./routes/User");
